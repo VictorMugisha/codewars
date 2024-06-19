48ee2d5370604365dbe99d89a478e09b284008a6
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-06-19 16:07:29 +0200

    Find Unique String [Works for few cases

--- findUniqueString/sctipt.js
+++ findUniqueString/sctipt.js
@@ -1,7 +1,18 @@
 
 function findUniq(arr) {
     const normalize = arr.map(element => element.toLowerCase())
-    
+    for (let index in normalize) {
+        let word = normalize[index]
+        let copy = [...normalize]
+        for (let char of word) {
+            copy.splice(index, 1).join('')
+            if (copy.filter(c => c.includes(char)).length > 1) {
+                break
+            } else {
+                return arr[index]
+            }
+        }
+    }
 }
 
-findUniq([ 'Aa', 'aaa', 'aaaaa', 'BbBb', 'Aaaa', 'AaAaAa', 'a' ])
+conoslfindUniq([ 'Aa', 'aaa', 'aaaaa', 'BbBb', 'Aaaa', 'AaAaAa', 'a' ])
