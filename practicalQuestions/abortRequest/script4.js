450a7045798b9f342be271b950f66e1f38c51102
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-06-21 17:04:15 +0200

    Added Other Mental Modal Questions

--- practicalQuestions/abortRequest/script4.js
+++ practicalQuestions/abortRequest/script4.js
@@ -3,10 +3,47 @@
  */
 const log = console.log
 
+// function fetchToDo() {
+//     const xhr = new XMLHttpRequest()
+//     xhr.open("GET", "https://jsonplaceholder.typicode.com/todos/1", true)
+//     xhr.send()
+//     xhr.onreadystatechange = () => {
+//         if (xhr.readyState === 4) {
+//             if (xhr.status >= 200 && xhr.status <= 300) {
+//                 const data = JSON.parse(xhr.response)
+//                 log(data)
+//             } else if (xhr.status >= 400 && xhr.status <= 500) {
+//                 log("ERROR: 404 End point not found!")
+//             } else {
+//                 log("ERROR: The Fetch Failed with the status code: ", xhr.status)
+//             }
+//         }
+//     }
+//     xhr.onerror = () => {
+//         log("Something wrong with the url!")
+//     }
+// }
+
+fetchToDo()
+
+/**
+ * Extend the `fetchToDo` function from Question 4 to include custom headers in the request. Specifically, you need to add a User-Agent header with a custom value and a Content-Type header set to application/json. Additionally, modify the function to send a JSON payload in the request body. After sending the request, the function should parse the JSON response and log the parsed object to the console.
+ */
+
 function fetchToDo() {
     const xhr = new XMLHttpRequest()
-    xhr.open("GET", "https://jsonplaceholder.typicode.com/todos/1", true)
-    xhr.send()
+    xhr.open("POST", "https://jsonplaceholder.typicode.com/todos", true)
+
+    xhr.setRequestHeader("User-Agent", "Victor")
+    xhr.setRequestHeader("Content-Type", "application/json")
+    
+    const payload = JSON.stringify({
+        userId: 200,
+        name: "Victor Mugisha"
+    })
+
+    xhr.send(payload)
+
     xhr.onreadystatechange = () => {
         if (xhr.readyState === 4) {
             if (xhr.status >= 200 && xhr.status <= 300) {
@@ -23,5 +60,3 @@ function fetchToDo() {
         log("Something wrong with the url!")
     }
 }
-
-fetchToDo()
